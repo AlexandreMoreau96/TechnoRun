16b06214d82f48398f0117fb9a17de400f8143cc
william <wil.beau@hotmail.com>
2019-11-30 18:04:50 -0500

    premier jet de generation procedural

---
+++ Assets/Script/LevelBuider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBuider : MonoBehaviour
+{
+    [SerializeField]
+    private LevelPart m_LevelPart;
+    private LevelPart m_LastLevelPartSpawn;
+    private int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_LastLevelPartSpawn = Instantiate(m_LevelPart.LevelTransform, m_LevelPart.LevelTransform.transform.position, Quaternion.identity).GetComponent<LevelPart>();
+        SpawnLevelPart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(count % 120 == 0)
+        {
+            SpawnLevelPart();
+        }
+        count += 1;
+    }
+
+    public void SpawnLevelPart()
+    {
+        m_LastLevelPartSpawn = Instantiate(m_LevelPart, m_LastLevelPartSpawn.EndPosition.position, Quaternion.identity).GetComponent<LevelPart>();
+    }
+}

41576587b61558dcda356a3cbd76b36003e94ffa
william <wil.beau@hotmail.com>
2019-12-01 14:55:32 -0500

    small gameplay

---
+++ Assets/Script/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private float m_Speed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 verMovement = transform.right;
+        Vector3 horMovement = -transform.forward * Input.GetAxisRaw("Horizontal");
+        Vector3 vel = (horMovement + verMovement).normalized * m_Speed;
+        transform.position += vel * Time.deltaTime;
+    }
+
+    internal void IncreaseSpeed()
+    {
+        m_Speed *= 1.5f;
+        m_Speed = m_Speed >= 35.0f ? 35.0f : m_Speed;
+    }
+}
--- Assets/Script/LevelBuider.cs
+++ Assets/Script/LevelBuider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,27 +7,52 @@ public class LevelBuider : MonoBehaviour
 {
     [SerializeField]
     private LevelPart m_LevelPart;
+    [SerializeField]
+    private GameObject m_PlayerPrefab;
+    private Transform m_Player;
+    private CharacterController m_CharacterController;
     private LevelPart m_LastLevelPartSpawn;
     private int count = 0;
+    private int m_PartCount = 0;
+    private bool m_NextPartSpawn = true;
+
     // Start is called before the first frame update
     void Start()
     {
+        m_Player = Instantiate(m_PlayerPrefab.transform, m_LevelPart.LevelTransform.transform.position, Quaternion.identity);
+        m_CharacterController = m_Player.GetComponent<CharacterController>();
         m_LastLevelPartSpawn = Instantiate(m_LevelPart.LevelTransform, m_LevelPart.LevelTransform.transform.position, Quaternion.identity).GetComponent<LevelPart>();
+        SpawnLevelPart();
+        SpawnLevelPart();
+        SpawnLevelPart();
+        SpawnLevelPart();
+        SpawnLevelPart();
         SpawnLevelPart();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(count % 120 == 0)
+        if(Vector3.Distance(m_Player.position, m_LastLevelPartSpawn.EndPosition.position) < 50)
         {
             SpawnLevelPart();
         }
-        count += 1;
+        if(m_PartCount % 10 == 0 && m_NextPartSpawn)
+        {
+            IncreaseSpeed();
+            m_NextPartSpawn = false;
+        }
+    }
+
+    private void IncreaseSpeed()
+    {
+        m_CharacterController.IncreaseSpeed();
     }
 
     public void SpawnLevelPart()
     {
         m_LastLevelPartSpawn = Instantiate(m_LevelPart, m_LastLevelPartSpawn.EndPosition.position, Quaternion.identity).GetComponent<LevelPart>();
+        m_NextPartSpawn = true;
+        m_PartCount += 1;
     }
 }
